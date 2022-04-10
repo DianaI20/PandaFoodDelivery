7b1ecd6b9175e96747efdbbc5a0bf198d7d4d940
Diana <dianaiordan89@yahoo.com>
2022-04-10 15:58:32 +0300

    Initial commit

---
+++ src/main/java/ro/utcluj/pandafooddelivery/PandaFoodDeliveryApplication.java
@@ -0,0 +1,13 @@
+package ro.utcluj.pandafooddelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaFoodDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PandaFoodDeliveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/utcluj/pandafooddelivery/PandaFoodDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.utcluj.pandafooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PandaFoodDeliveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
